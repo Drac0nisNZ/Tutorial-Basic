568cd146f2ebb0e798642173edb42dbace991c21
NathanielGeiger <Nathaniel-Geiger@live.nmit.ac.nz>
2024-09-25 10:07:42 +1200

    adding config

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBWtulBzHkOxworPgtiuI-_exBxgNMDGYw",
+  authDomain: "tutorial-f57d3.firebaseapp.com",
+  projectId: "tutorial-f57d3",
+  storageBucket: "tutorial-f57d3.appspot.com",
+  messagingSenderId: "276679842503",
+  appId: "1:276679842503:web:5af1e19bc2f6669f45bce2",
+  measurementId: "G-R9EGWBCKRP"
+};
+
+export const app = initializeApp(firebaseConfig);
